d26795fcd7f0834fd83d2ec5c40d25304a10175d
Dylan Gatlin <dgatlin5@gmail.com>
2022-12-02 19:21:13 -0700

    asyncio template

--- aoc/day_0.py
+++ aoc/day_0.py
@@ -2,35 +2,23 @@
 
 import re
 import click
+import asyncio
 
 import numpy as np
 
 from pathlib import Path
+from typing import Union
 
 
-@click.command()
-@click.argument("inp", default=0)
-@click.option("-v", "--verbose", count=True)
-@click.option("-t", "--test", is_flag=True)
-def main(inp, verbose, test) -> int:
-    proj = Path(__file__).absolute().parent.parent
-    day = re.search(r"\d+", Path(__file__).name)
-    day = day.group(0) if day else ""
-    day = day if day else ""
-    if test:
-        in_file = proj / f"dat/day_{day}_test.txt"
-    else:
-        in_file = proj / f"dat/day_{day}.txt"
-    if verbose:
-        print(f"Advent of Code Day {day}")
+async def from_file(file_name: Union[str, Path], format, test: bool = False):
+    in_file = Path(file_name) if isinstance(file_name, str) else file_name
 
     with in_file.open('r') as fil:
-        lines = fil.read().splitlines()  # There is no "\n" in these
+        lines = fil.read().splitlines()  # there is no "\n" in these
         if len(lines) == 1:
             inputs = lines[0]
         else:
-            form = "uk"
-            match form:
+            match format:
                 case "newline ints":
                     inputs = []
                     for line in lines:
@@ -71,10 +59,31 @@ def main(inp, verbose, test) -> int:
                     for line in lines:
                         inputs.append(list(line))
                     inputs = inputs == "1"
-                case "int":
-                    inputs = inp  # From command line arguments
-                case _:
+                case "unknown":
                     inputs = lines
+                case _:
+                    inputs = format
+
+    return inputs
+
+
+@click.command()
+@click.argument("format", default=0)
+@click.option("-v", "--verbose", count=True)
+@click.option("-t", "--test", is_flag=True)
+def main(format, verbose, test) -> int:
+    proj = Path(__file__).absolute().parent.parent
+    day = re.search(r"\d+", Path(__file__).name)
+    day = day.group(0) if day else ""
+    day = day if day else ""
+    if test:
+        in_file = proj / f"dat/day_{day}_test.txt"
+    else:
+        in_file = proj / f"dat/day_{day}.txt"
+    if verbose:
+        print(f"Advent of Code Day {day}")
+
+    inputs = asyncio.run(from_file(in_file, format, test))
 
     part_1 = ""
     print(f"Part 1: {part_1}")
@@ -85,4 +94,4 @@ def main(inp, verbose, test) -> int:
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
