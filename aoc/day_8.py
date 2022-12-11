d1b758c2fd37eeeb2796fc419acb03bd97632501
Dylan Gatlin <dgatlin5@gmail.com>
2022-12-11 00:28:31 -0700

    Days 7 and 8

--- aoc/day_8.py
+++ aoc/day_8.py
@@ -8,6 +8,74 @@ import asyncclick as click
 
 from pathlib import Path
 from typing import Union
+from numba import njit, prange
+
+
+# @njit()
+def count_hidden(tree_grid, verbose=0):
+    hidden = 0
+    for i in prange(len(tree_grid)):
+        for j in prange(len(tree_grid[i])):
+            above = False
+            below = False
+            left = False
+            right = False
+            for ab in prange(j):
+                above = above or tree_grid[i, j] <= tree_grid[i, ab]
+            for be in prange(j+1, len(tree_grid[0])):
+                below = below or tree_grid[i, j] <= tree_grid[i, be]
+            for le in prange(i):
+                left = left or tree_grid[i, j] <= tree_grid[le, j]
+            for ri in prange(i + 1, len(tree_grid)):
+                right = right or tree_grid[i, j] <= tree_grid[ri, j]
+            is_hidden = above and below and left and right
+            hidden += is_hidden
+    return hidden
+
+
+def score_views(tree_grid, verbose=0):
+    views = np.ones(tree_grid.shape)
+    for i in prange(tree_grid.shape[0]):
+        for j in prange(tree_grid.shape[1]):
+            if verbose:
+                print(i, j, tree_grid[i, j])
+            for k in range(j - 1, -1, -1):
+                if tree_grid[i, k] >= tree_grid[i, j]:
+                    if verbose:
+                        print(f"    left: {j - k}, {j} {k}")
+                    views[i, j] *= j - k
+                    break
+            else:
+                if verbose:
+                    print(f"    left: {j}")
+                views[i, j] *= j
+            for k in range(j + 1, tree_grid.shape[1], 1):
+                if tree_grid[i, k] >= tree_grid[i, j]:
+                    if verbose:
+                        print(f"    right: {k - j}")
+                    views[i, j] *= k - j
+                    break
+            else:
+                views[i, j] *= tree_grid.shape[1] - j - 1
+            for k in range(i - 1, -1, -1):
+                if tree_grid[k, j] >= tree_grid[i, j]:
+                    if verbose:
+                        print(f"    above: {i - k}")
+                    views[i, j] *= i - k
+                    break
+            else:
+                views[i, j] *= i
+            for k in range(i + 1, tree_grid.shape[0], 1):
+                if tree_grid[k, j] >= tree_grid[i, j]:
+                    if verbose:
+                        print(f"    below: {k - i}")
+                    views[i, j] *= k - i
+                    break
+            else:
+                views[i, j] *= tree_grid.shape[0] - i - 1
+            if verbose:
+                print(f"    final: {views[i, j]}")
+    return views
 
 
 async def aoc_from_file(file_name: Union[str, Path], form, inp):
@@ -94,13 +162,18 @@ async def main(inp, verbose, test) -> int:
     if verbose:
         print(f"Advent of Code Day {day}")
 
-    form = "single string"
+    form = "int grid"
     inputs = await asyncio.create_task(aoc_from_file(in_file, form, inp))
 
-    part_1 = ""
+    hidden = count_hidden(inputs, verbose=verbose)
+
+    part_1 = inputs.size - hidden
     print(f"Part 1: {part_1}")
-    part_2 = ""
-    print(f"Part 2: {part_2}")
+    views = score_views(inputs, verbose)
+    if verbose:
+        print(views)
+    part_2 = views.max()
+    print(f"Part 2: {part_2:.0f}")
 
     return 0
 
