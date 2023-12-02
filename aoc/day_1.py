da2c1f63f25c8bca0b6a4d40f6e370620323aaed
Dylan Gatlin <dgatlin5@gmail.com>
2023-12-02 09:20:00 -0700

    init 2023

--- aoc/day_1.py
+++ aoc/day_1.py
@@ -1,18 +1,92 @@
 #!/usr/bin/env python3
 
 import re
-import click
+import tqdm
+import asyncio
 
 import numpy as np
+import asyncclick as click
 
 from pathlib import Path
+from typing import Union
+from pprint import pprint
+
+
+async def aoc_from_file(file_name: Union[str, Path], form, inp):
+    in_file = Path(file_name) if isinstance(file_name, str) else file_name
+
+    with in_file.open('r') as fil:
+        lines = fil.read().splitlines()  # there is no "\n" in these
+        match form:
+            case "newline ints":
+                inputs = []
+                for line in lines:
+                    inputs.append(int(line))
+                inputs = np.array(inputs)
+            case "newline int bundles":
+                inputs = []
+                chunk = []
+                for line in lines:
+                    if line == "":
+                        inputs.append(chunk)
+                        chunk = []
+                    else:
+                        chunk.append(int(line))
+            case "newline strings":
+                inputs = []
+                chunk = []
+                for line in lines:
+                    if line == "":
+                        inputs.append(chunk)
+                        chunk = []
+                    else:
+                        chunk.append(line)
+                inputs.append(chunk)
+            case "comma paired lines":
+                inputs = []
+                for line in lines:
+                    inputs.append(line.split(","))
+            case "space paired lines":
+                inputs = []
+                for line in lines:
+                    inputs.append(line.split())
+            case "comma separated":
+                inputs = line.split(",")
+            case "comma grid":
+                inputs = []
+                for line in lines:
+                    inputs.append(line.split(","))
+                inputs = np.array(inputs)
+            case "hash grid":
+                inputs = []
+                for line in lines:
+                    inputs.append(list(line))
+                inputs = inputs == "#"
+            case "binary grid":
+                inputs = []
+                for line in lines:
+                    inputs.append(list(line))
+                inputs = np.array(inputs) == "1"
+            case "int grid":
+                inputs = []
+                for line in lines:
+                    inputs.append(list(line))
+                inputs = np.array(inputs)
+            case "single string":
+                inputs = lines
+            case "int":
+                inputs = inp
+            case _:
+                inputs = format
+
+    return inputs
 
 
 @click.command()
 @click.argument("inp", default=0)
 @click.option("-v", "--verbose", count=True)
 @click.option("-t", "--test", is_flag=True)
-def main(inp, verbose, test) -> int:
+async def main(inp, verbose, test) -> int:
     proj = Path(__file__).absolute().parent.parent
     day = re.search(r"\d+", Path(__file__).name)
     day = day.group(0) if day else ""
@@ -24,36 +98,16 @@ def main(inp, verbose, test) -> int:
     if verbose:
         print(f"Advent of Code Day {day}")
 
-    with in_file.open('r') as fil:
-        lines = fil.read().splitlines()
-        if len(lines) == 1:
-            input_text = lines[0]
-        else:
-            inputs = lines
+    form = "single string"
+    inputs = await asyncio.create_task(aoc_from_file(in_file, form, inp))
 
-    cals = []
-    elf = []
-    for line in inputs:
-        if line == "":
-            cals.append(elf)
-            elf = []
-        else:
-            elf.append(int(line))
-    part_1 = 0
-    for elf in cals:
-        part_1 = max(np.sum(elf), part_1)
-
+    part_1 = ""
     print(f"Part 1: {part_1}")
-    sums = []
-    for elf in cals:
-        sums.append(np.sum(elf))
-    sums = np.array(sums)
-    sums.sort()
-    part_2 = sums[-3:].sum()
+    part_2 = ""
     print(f"Part 2: {part_2}")
 
     return 0
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
