ced04ba3cd02dd7efc9ea8dc496c4854e46c84f5
Dylan Gatlin <dgatlin5@proton.me>
2023-12-02 21:54:14 -0700

    Init day 3

---
+++ aoc/day_3.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+"""This is a template for an Advent of Code day
+"""
+
+import asyncio
+import re
+from pathlib import Path
+from pprint import pprint
+
+import asyncclick as click
+import numpy as np
+from rich.progress import track
+
+
+async def aoc_from_file(
+    file_name: str | Path, form: str, inp: int, custom_function=None
+):
+    """Reads an Advent of Code input file according to the given format
+
+    Args:
+        file_name (str | Path): The input file path
+        form (str): A supported format to be read
+        inp (int): In the case where a single int is given as input, returns
+            that, useful for preserving overall structure
+        custom_function (callable, optional): If form="custom", uses this
+            function to process the input text. Defaults to None.
+
+    Raises:
+        ValueError: For unsupported outputs
+
+    Returns:
+        str | list[str] | list[list[str]] | np.ndarray: A logical output
+            datatype for the given input and form
+    """
+    in_file = Path(file_name) if isinstance(file_name, str) else file_name
+
+    with in_file.open("r") as fil:
+        lines = fil.read().splitlines()  # there is no "\n" in these
+        match form:
+            case "newline ints":
+                inputs = []
+                for line in lines:
+                    inputs.append(int(line))
+                inputs = np.array(inputs)
+            case "newline int bundles":
+                inputs = []
+                chunk = []
+                for line in lines:
+                    if line == "":
+                        inputs.append(chunk)
+                        chunk = []
+                    else:
+                        chunk.append(int(line))
+            case "newline strings":
+                inputs = []
+                chunk = []
+                for line in lines:
+                    if line == "":
+                        inputs.append(chunk)
+                        chunk = []
+                    else:
+                        chunk.append(line)
+                inputs.append(chunk)
+            case "comma paired lines":
+                inputs = []
+                for line in lines:
+                    inputs.append(line.split(","))
+            case "space paired lines":
+                inputs = []
+                for line in lines:
+                    inputs.append(line.split())
+            case "comma separated":
+                inputs = line.split(",")
+            case "comma grid":
+                inputs = []
+                for line in lines:
+                    inputs.append(line.split(","))
+                inputs = np.array(inputs)
+            case "hash grid":
+                inputs = []
+                for line in lines:
+                    inputs.append(list(line))
+                inputs = inputs == "#"
+            case "binary grid":
+                inputs = []
+                for line in lines:
+                    inputs.append(list(line))
+                inputs = np.array(inputs) == "1"
+            case "int grid":
+                inputs = []
+                for line in lines:
+                    inputs.append(list(line))
+                inputs = np.array(inputs)
+            case "single string":
+                inputs = lines
+            case "int":
+                inputs = inp
+            case "custom":
+                if custom_function is not None:
+                    inputs = custom_function(lines)
+                else:
+                    raise ValueError("Custom function undefined")
+            case _:
+                inputs = format
+
+    return inputs
+
+
+@click.command()
+@click.argument("inp", default=0)
+@click.option("-v", "--verbose", count=True)
+@click.option("-t", "--test", is_flag=True)
+async def main(inp, verbose, test) -> int:
+    proj = Path(__file__).absolute().parent.parent
+    day = re.search(r"\d+", Path(__file__).name)
+    day = day.group(0) if day else ""
+    day = day if day else ""
+    if test:
+        in_file = proj / f"dat/day_{day}_test.txt"
+    else:
+        in_file = proj / f"dat/day_{day}.txt"
+    if verbose:
+        print(f"Advent of Code Day {day}")
+
+    form = "single string"
+    inputs = await asyncio.create_task(aoc_from_file(in_file, form, inp))
+
+    part_1 = ""
+    print(f"Part 1: {part_1}")
+    part_2 = ""
+    print(f"Part 2: {part_2}")
+
+    return 0
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- tests/test_all_days.py
+++ tests/test_all_days.py
@@ -1,21 +1,23 @@
-import sys
-import pytest
-
 import subprocess as sub
-
+import sys
 from pathlib import Path
+
+import pytest
 from astropy.time import Time
 
-@pytest.mark.all
+
 def test_all_days():
     proj_dir = Path(__file__).absolute().parent.parent
     test_log = Path("./test_log.txt")
-    with test_log.open('w') as log:
-        log.write(proj_dir.as_posix() + '\n')
+    with test_log.open("w") as log:
+        log.write(proj_dir.as_posix() + "\n")
         for fil in sorted((proj_dir / "aoc").glob("day_*.py")):
             start = Time.now()
-            p = sub.run(" ".join([sys.executable, fil.absolute().as_posix()]),
-                        capture_output=True, shell=True)
+            p = sub.run(
+                " ".join([sys.executable, fil.absolute().as_posix()]),
+                capture_output=True,
+                shell=True,
+            )
             dt = Time.now() - start
             log.write(f"{p.returncode} {dt.sec:.3f}s {fil.as_posix()}\n")
             log.write(p.stdout.decode("utf-8"))
@@ -25,8 +27,9 @@ def test_all_days():
 def test_all_days_verbose():
     proj_dir = Path(__file__).absolute().parent.parent
     for fil in sorted((proj_dir / "aoc").glob("day_*.py")):
-        p = sub.run(" ".join(
-            [sys.executable, fil.absolute().as_posix(), "-v"]
-        ),
-            capture_output=True, shell=True)
+        p = sub.run(
+            " ".join([sys.executable, fil.absolute().as_posix(), "-v"]),
+            capture_output=True,
+            shell=True,
+        )
         p.check_returncode()
